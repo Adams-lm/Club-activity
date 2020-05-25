084fe0d38678283807c6dc5650dc644b5bc80fd4
Admas-lm <865725198@qq.com>
2020-05-25 23:22:59 +0800

    修改login.php

--- code/php/login.php
+++ code/php/login.php
@@ -10,18 +10,15 @@
     if($identity=="user"){
         //查询用户id，昵称和密码，前两者存入session
         $sql="SELECT user_id,user_name,password,status from user WHERE account=?;";
-        $stmt=mysqli_prepare($conn,$sql);
-        mysqli_stmt_bind_param($stmt,"s",$account);
-        mysqli_stmt_execute($stmt);
-        $rs=mysqli_stmt_get_result($stmt);
     }
     else if($identity=="admin"){
         $sql="SELECT admin_id,account,password from admin WHERE account=?;";
-        $stmt=mysqli_prepare($conn,$sql);
-        mysqli_stmt_bind_param($stmt,"s",$account);
-        mysqli_stmt_execute($stmt);
-        $rs=mysqli_stmt_get_result($stmt);
     }
+    $stmt=mysqli_prepare($conn,$sql);
+    mysqli_stmt_bind_param($stmt,"s",$account);
+    mysqli_stmt_execute($stmt);
+    $rs=mysqli_stmt_get_result($stmt);
+
     if(!$row=mysqli_fetch_array($rs)){//若无返回结果
         page_redirect(0,"","用户名不存在！");
         die();
@@ -30,9 +27,9 @@
         if($row[2]==$password){
             $_SESSION["userid"]=$row[0];
             $_SESSION["username"]=$row[1];
-            $status=$row[3];
             //用户界面
             if($identity=="user"){
+                $status=$row[3];
                 if($status==1){
                 page_redirect(1,"","登陆成功！");
                 }
@@ -49,7 +46,6 @@
             }
         }
         else{
-            die("密码错误");
             page_redirect(0,"","密码错误！");
         }
     }
