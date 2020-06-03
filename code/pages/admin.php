86f84d6d7fb16876135217a5934453f0cb02fa77
Admas-lm <865725198@qq.com>
2020-06-03 19:44:50 +0800

    乱改1.0

--- code/pages/admin.php
+++ code/pages/admin.php
@@ -1,106 +1,107 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
 
-    <title>社团活动管理系统</title>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-    <!-- Bootstrap core CSS -->
-    <link href="../css/bootstrap.css" rel="stylesheet">
+  <title>社团活动管理系统</title>
 
-    <!-- Add custom CSS here -->
-    <link href="../css/sb-admin.css" rel="stylesheet"><!-- 侧边栏 -->
-    <link rel="stylesheet" href="../font-awesome/css/font-awesome.min.css"><!-- icon_font -->
-    <!-- Page Specific CSS -->
-    <link rel="stylesheet" href="../css/morris-0.4.3.min.css">
-    <!-- sddr写的 -->
-    <link rel="stylesheet" href="../css/mystyle_index.css">
-  </head>
+  <!-- Bootstrap core CSS -->
+  <link href="../css/bootstrap.css" rel="stylesheet">
 
-  <body>
-    <div class="row">
-      <div class="col-md-2">
-        <div id="wrapper">
+  <!-- Add custom CSS here -->
+  <link href="../css/sb-admin.css" rel="stylesheet"><!-- 侧边栏 -->
+  <link rel="stylesheet" href="../font-awesome/css/font-awesome.min.css"><!-- icon_font -->
+  <!-- Page Specific CSS -->
+  <link rel="stylesheet" href="../css/morris-0.4.3.min.css">
+  <!-- sddr写的 -->
+  <link rel="stylesheet" href="../css/mystyle_index.css">
+</head>
 
-          <!-- Sidebar -->
-          <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="admin.php">社团名称</a>
-            </div>
+<body>
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse navbar-ex1-collapse">
-              <ul class="nav navbar-nav side-nav">
+  <div id="wrapper">
 
-                <li class="active"><a href="admin.php"><i class="fa fa-dashboard"></i> 首  页</a></li>
+    <!-- Sidebar -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="admin.php">社团名称</a>
+      </div>
 
-                <li class="dropdown">
-                  <a href="admin/activityManagement/addActivity.php" target="iframe_link" class="dropdown-toggle" data-toggle="dropdown" >
-                    <i class="fa fa-caret-square-o-down"></i> 管理社团活动 <b class="caret"></b></a>
-                  <ul class="dropdown-menu">
-                    <li><a href="admin/activityManagement/addActivity.php" target="iframe_link">发布社团活动</a></li>
-                    <li><a href="admin/activityManagement/approveActJoin.php" target="iframe_link">审批报名申请</a></li>
-                    <li><a href="admin/activityManagement/manageActInfo.php" target="iframe_link">管理活动信息</a></li>
-                  </ul>
-                </li>
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse navbar-ex1-collapse">
+        <ul class="nav navbar-nav side-nav">
 
-                <li class="dropdown">
-                  <a href="admin/userManagement/approveAccount.php" target="iframe_link" class="dropdown-toggle" data-toggle="dropdown">
-                    <i class="fa fa-caret-square-o-down"></i> 管理用户信息 <b class="caret"></b></a>
-                  <ul class="dropdown-menu">
-                    <li><a href="admin/userManagement/approveAccount.php" target="iframe_link">审批注册用户</a></li>
-                    <li><a href="admin/userManagement/manageAccount.php" target="iframe_link">管理在册用户</a></li>
-                  </ul>
-                </li>
-              </ul>
-              <!-- 侧边栏结束 -->
+          <li class="active"><a href="admin.php"><i class="fa fa-dashboard"></i> 首 页</a></li>
 
-              <!-- 顶栏开始   -->
-              <ul class="nav navbar-nav navbar-right navbar-user">
-                <li class="dropdown user-dropdown">
-                  
-                  <a href="#" class="dropdown-toggle" style="padding: 10px 40px;"data-toggle="dropdown">
-                    <img src="../images/QQ.png" alt="" style="width: 28px;height: 28px;border-radius: 20px;margin-right: 10px;">
-                    <i class="fa fa-user"></i> 用户名 <b class="caret"></b></a>
-                  <ul class="dropdown-menu">
-                    <li><a href="#"><i class="fa fa-user"></i> 账号设置</a></li>
-                    <li><a href="#"><i class="fa fa-envelope"></i> 消息 <span class="badge">7</span></a></li>
-                    <li><a href="#"><i class="fa fa-gear"></i> 帮助</a></li>
-                    <li class="divider"></li>
-                    <li><a href="#"><i class="fa fa-power-off"></i> 退出</a></li>
-                  </ul>
-                </li>
-              </ul><!-- 顶栏结束 --> 
-            </div><!-- /.navbar-collapse -->
-          </nav>
-        </div><!-- /#wrapper -->
-      </div><!-- /.col-md-2 -->
+          <li class="dropdown">
+            <a href="admin/activityManagement/addActivity.php" target="iframe_link" class="dropdown-toggle" data-toggle="dropdown">
+              <i class="fa fa-caret-square-o-down"></i> 管理社团活动 <b class="caret"></b></a>
+            <ul class="dropdown-menu">
+              <li><a href="admin/activityManagement/addActivity.php" target="iframe_link">发布社团活动</a></li>
+              <li><a href="admin/activityManagement/approveActJoin.php" target="iframe_link">审批报名申请</a></li>
+              <li><a href="admin/activityManagement/manageActInfo.php" target="iframe_link">管理活动信息</a></li>
+            </ul>
+          </li>
 
-      <div class="col-md-10"><!-- scrolling="no"  -->
-        <iframe src="" name="iframe_link"></iframe>
-      </div><!-- /.col-md-10 -->
+          <li class="dropdown">
+            <a href="admin/userManagement/approveAccount.php" target="iframe_link" class="dropdown-toggle" data-toggle="dropdown">
+              <i class="fa fa-caret-square-o-down"></i> 管理用户信息 <b class="caret"></b></a>
+            <ul class="dropdown-menu">
+              <li><a href="admin/userManagement/approveAccount.php" target="iframe_link">审批注册用户</a></li>
+              <li><a href="admin/userManagement/manageAccount.php" target="iframe_link">管理在册用户</a></li>
+            </ul>
+          </li>
+        </ul>
+        <!-- 侧边栏结束 -->
 
-    </div><!-- /.row -->
+        <!-- 顶栏开始   -->
+        <ul class="nav navbar-nav navbar-right navbar-user">
+          <li class="dropdown user-dropdown">
 
-    <!-- JavaScript -->
-    <script src="../js/jquery-1.10.2.js"></script>
-    <script src="../js/bootstrap.js"></script>
+            <a href="#" class="dropdown-toggle" style="padding: 10px 40px;" data-toggle="dropdown">
+              <img src="../images/QQ.png" alt="" style="width: 28px;height: 28px;border-radius: 20px;margin-right: 10px;">
+              <i class="fa fa-user"></i> 用户名 <b class="caret"></b></a>
+            <ul class="dropdown-menu">
+              <li><a href="#"><i class="fa fa-user"></i> 账号设置</a></li>
+              <li><a href="#"><i class="fa fa-envelope"></i> 消息 <span class="badge">7</span></a></li>
+              <li><a href="#"><i class="fa fa-gear"></i> 帮助</a></li>
+              <li class="divider"></li>
+              <li><a href="#"><i class="fa fa-power-off"></i> 退出</a></li>
+            </ul>
+          </li>
+        </ul><!-- 顶栏结束 -->
+      </div><!-- /.navbar-collapse -->
+    </nav>
 
-    <!-- Page Specific Plugins -->    <script src="js/raphael-min.js"></script>
-    <script src="../js/morris-0.4.3.min.js"></script>
-    <script src="../js/morris/chart-data-morris.js"></script>
-    <script src="../js/tablesorter/jquery.tablesorter.js"></script>
-    <script src="../js/tablesorter/tables.js"></script>
 
-  </body>
+    <div id="page-wrappper">
+      <iframe src="" name="iframe_link"></iframe>
+    </div>
+
+  </div>
+
+  <!-- JavaScript -->
+  <script src="../js/jquery-1.10.2.js"></script>
+  <script src="../js/bootstrap.js"></script>
+
+  <!-- Page Specific Plugins -->
+  <script src="js/raphael-min.js"></script>
+  <script src="../js/morris-0.4.3.min.js"></script>
+  <script src="../js/morris/chart-data-morris.js"></script>
+  <script src="../js/tablesorter/jquery.tablesorter.js"></script>
+  <script src="../js/tablesorter/tables.js"></script>
+
+</body>
+
 </html>
